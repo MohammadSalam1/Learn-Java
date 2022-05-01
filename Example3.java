aaeaf52d98500ab87f61163bcfb124c025ff2fd5
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-01 15:46:13 +0200

    differnce between int and doube

--- Example3.java
+++ Example3.java
@@ -1,5 +1,24 @@
+/*
+        this program will illustrates the differnce
+        between int and double
+        */
+
 class Example3 {
     public static void main(String[] args) {
-        
+        int v; // int variable
+        double x; //floating-point (double) variable
+
+        v = 10; //assigning value for int v
+        x = 10.0; //assigning value for double x
+
+        System.out.println("Original value of v: " + v);
+        System.out.println("Original value of x: " + x);
+        System.out.println(); // just to print a blank line in between
+
+        v = v / 4;
+        x = x / 4;
+
+        System.out.println("V value after division: " + v);
+        System.out.println("X value after division: " + x);
     }
 }
