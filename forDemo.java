616ee24e93a273fea8b094f8e1a957a06e13e8ef
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-04 16:25:09 +0200

    using for loop

---
+++ forDemo.java
@@ -0,0 +1,11 @@
+public class forDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for(count = 0; count < 5; count++){
+            System.out.println("counting: " + count);
+
+        }
+        System.out.println("Done counting");
+    }
+}
