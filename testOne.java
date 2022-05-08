826b0f78b1697c5c0685e50dcb585b4edbe5f277
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-08 21:49:40 +0200

    weightOnMoonTest

---
+++ testOne.java
@@ -0,0 +1,9 @@
+/*
+The moon gravity is about 17 percent that of earth's.
+Write a program that computes your effective weight on the moon*/
+public class testOne {
+    public static void main(String[] args) {
+
+
+    }
+}
