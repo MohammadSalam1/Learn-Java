16d1b209a360bc6412cf8bb506068cb27d403371
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-08 21:57:43 +0200

    moonWeight-testOne

--- testOne.java
+++ testOne.java
@@ -3,6 +3,12 @@ The moon gravity is about 17 percent that of earth's.
 Write a program that computes your effective weight on the moon*/
 public class testOne {
     public static void main(String[] args) {
+        double earthWeight, moonWeight;
+        earthWeight = 80;
+
+        moonWeight = earthWeight * 0.17;
+
+        System.out.println("my weight on the moon is: " + moonWeight + " kgs");
 
 
     }
