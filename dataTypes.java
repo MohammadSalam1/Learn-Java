89b589ed3d437e85f4f0ad0dd6fb913c8e60ff07
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-10 21:49:41 +0200

    added comments to dataTypes

--- dataTypes.java
+++ dataTypes.java
@@ -1,2 +1,16 @@
 public class dataTypes {
+    public static void main(String[] args) {
+        /*
+        There are eight primitive data types in java
+        Those data types are
+        * boolean: True/False
+        * byte: 8-bit integer
+        * char: character
+        * double: double-precision floating-point
+        * float: single-precision floating-point
+        * int: integer
+        * long: long integer
+        * short: short integer
+         */
+    }
 }
