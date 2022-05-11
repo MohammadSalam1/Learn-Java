01b8a8334b82b8a67494e08ca97cd8fd652de8a2
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-11 12:35:00 +0200

    soundHW

---
+++ soundHW.java
@@ -0,0 +1,15 @@
+/*
+compute the distance to a lightning
+strike whose sound take 7.2 seconds
+to reach you
+*/
+public class soundHW {
+    public static void main(String[] args) {
+        double dist; //dist for distance
+
+        dist = 7.2 * 1100;
+
+        System.out.println("the lightning is: " + dist + " feet away");
+
+    }
+}
