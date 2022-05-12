77860233268d4913de29179f2906a67457cbf635
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-12 15:28:46 +0200

    escape sequences with java

---
+++ strDemo.java
@@ -0,0 +1,10 @@
+//escape sequences
+public class strDemo {
+    public static void main(String[] args) {
+
+        System.out.println("first line \nsecond line"); // \n whatever output comes after will be in a new line
+        System.out.println("A \tB \tC");
+        System.out.println("D \tE \tF"); // the \t is a tap space to align output
+
+    }
+}
