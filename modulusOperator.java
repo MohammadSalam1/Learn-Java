ad61f2a0fa8fbcc50f956b5ee32e44a421fa7f9b
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-14 18:27:06 +0200

    ModulusOperator

--- modulusOperator.java
+++ modulusOperator.java
@@ -7,8 +7,8 @@ public class modulusOperator {
         results = 10 / 3;
         irem = 10 % 3;
 
-        dresults = 10 / 3;
-        drem = 10 % 3;
+        dresults = 10.0 / 3.0;
+        drem = 10.0 % 3.0;
 
         System.out.println("Results and remainders of 10 / 3: " + results + " " + irem);
         System.out.println("Results and remainders of 10 / 3: " + dresults + " " + drem);
