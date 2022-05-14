4cf05f8b6f7098a5175fd6c386cca03c518ad0dd
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-14 16:30:22 +0200

    varIniDemo

---
+++ varIniDemo.java
@@ -0,0 +1,15 @@
+//lifetime of a variable
+public class varIniDemo {
+    public static void main(String[] args) {
+
+        int x;
+        for (x = 0; x < 3; x++) {
+
+            int y = -1;//y is initialized each time block is entered
+            System.out.println("y is: " + y);//this will always print -1
+            y = 100;
+            System.out.println("y is now: " + y);
+
+        }
+    }
+}
