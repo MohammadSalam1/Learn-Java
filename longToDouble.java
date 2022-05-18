af94446fc800d3ca03cfd44b0abed99d0f485d23
Mohammad Salam <mhd.salam99@gmail.com>
2022-05-18 16:42:13 +0200

    varConversion_longToDouble

---
+++ longToDouble.java
@@ -0,0 +1,10 @@
+public class longToDouble {
+    public static void main(String[] args) {
+        long L;
+        double D;
+
+        L = 2654846L;
+        D = L;
+        System.out.println("L and D are: " + L + " " + D);
+    }
+}
