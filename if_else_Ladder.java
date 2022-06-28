a6c7a4a349de2f0f686095ca968428460704c98a
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:55:37 +0200

    if_else_ladder

--- if_else_Ladder.java
+++ if_else_Ladder.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class if_else_Ladder {
+public class if_else_Ladder {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 6; x++) {
+            if (x == 1) {
+                System.out.println("X is one");
+            } else if (x == 2) {
+                System.out.println("X is two");
+            } else if (x == 3) {
+                System.out.println("X is three");
+            } else if (x == 4) {
+                System.out.println("X is four");
+            } else {
+                System.out.println("X is not between one and four"); //this is the default statement encase none of the
+                                                                    //above was true.
+            }
+        }
+    }
 }
