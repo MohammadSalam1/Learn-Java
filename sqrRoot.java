e4ddfb76f6e3cec289342cfda297f92c12a45b95
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:50:42 +0200

    sqrRootUpdate

--- sqrRoot.java
+++ sqrRoot.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class sqrRoot {
+public class sqrRoot {
+    public static void main(String[] args) {
+        double num, sroot, rerr; //num = number, sroot = squareRoot, rerr = rounding error;
+
+        for(num = 1.0; num < 100.0; num++) {
+            sroot = Math.sqrt(num);
+            System.out.println("square root of " + num + "is " + sroot);
+
+            //compute rounding error
+            rerr = num - (sroot * sroot);
+            System.out.println("roudning error is: " + rerr);
+            System.out.println();
+        }
+
+    }
 }
