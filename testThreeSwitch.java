4a891b571b1f4432dc32f0759bccae4ce09e6d91
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:52:11 +0200

    testThreeSwitchUpdated

--- testThreeSwitch.java
+++ testThreeSwitch.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class testThreeSwitch {
+import java.io.IOException;
+
+public class testThreeSwitch {
+    public static void main(String[] args) throws IOException {
+
+        char choice;
+
+        System.out.println("Help On:");
+        System.out.println(" 1. if");
+        System.out.println(" 2. switch");
+        System.out.println("Choose one: ");
+
+        choice = (char) System.in.read();
+
+        System.out.println("\n");
+
+        switch (choice) {
+            case '1':
+                System.out.println("the if: \n");
+                System.out.println("if (condition){statement};");
+                System.out.println("else {statement};");
+                break;
+            case '2':
+                System.out.println("the switch: \n");
+                System.out.println("switch(expression){");
+                System.out.println("case constant;");
+                System.out.println("    statement sequence");
+                System.out.println("break;");
+                System.out.println("}");
+                break;
+            default:
+                System.out.println("Selection not found");
+
+        }
+    }
 }
