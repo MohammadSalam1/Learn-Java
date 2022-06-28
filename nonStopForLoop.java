8bbfa08d9a9068cdc7c7d33104a63110171d5e3f
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:51:28 +0200

    nonStopForLoopUpdated

--- nonStopForLoop.java
+++ nonStopForLoop.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class nonStopForLoop {
+import java.io.IOException;
+
+public class nonStopForLoop {
+    public static void main(String[] args) throws IOException {
+        //this for loop won't stop unless you press S;
+
+        int i;
+        System.out.println("Press S to stop.");
+        System.out.println();
+
+        for(i = 0; (char) System.in.read() != 'S'; i++) //Has to be capital letter S;
+        System.out.println("Pass #" + i);
+    }
 }
