14fb09e1744844fd1327e35c4fd6786491781797
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:53:33 +0200

    guessNestedif

--- guessNestedif.java
+++ guessNestedif.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class guessNestedif {
+import java.io.IOException;
+
+public class guessNestedif {
+    public static void main(String[] args) throws IOException {
+
+        char ch, answer = 'k';
+        System.out.println("I'm thinking of a letter between A and Z!");
+        System.out.print("Can you guess what is it? ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer) {
+            System.out.println("You are Correct!");
+        } else {
+            System.out.print("Sorry, you're ");
+            if (ch < answer) {
+                System.out.println("too low");
+            } else {
+                System.out.println("too high");
+            }
+        }
+    }
 }
