b92893754043b551182bb4e338bd32c31d39e4d7
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-28 15:52:45 +0200

    switchDemoUpdated

--- switchDemo.java
+++ switchDemo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class switchDemo {
+public class switchDemo {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10; i++) {
+            switch (i) {
+                case 0 -> System.out.println("i is zero");
+                case 1 -> System.out.println("i is one");
+                case 2 -> System.out.println("i is two");
+                case 3 -> System.out.println("i is three");
+                case 4 -> System.out.println("i is four");
+                default -> System.out.println("i is five or more");
+            }
+        }
+    }
 }
