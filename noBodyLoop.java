19dfc257bd8fba0082700a7c883f8ed93a573368
Mohammad Salam <mhd.salam99@gmail.com>
2022-06-30 17:42:18 +0200

    noBodyLoop

---
+++ noBodyLoop.java
@@ -0,0 +1,12 @@
+public class noBodyLoop {
+    public static void main(String[] args) {
+
+        int i;
+        int sum =0;
+
+        for (i=0; i <=5; sum += i++);//sum of numbers through 5
+
+        System.out.println("Sum of numbers through 5 is: " + sum);
+
+    }
+}
