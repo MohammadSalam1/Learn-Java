8f48dcd61a9b08750f5c12496b7e33e42bb240f8
Mohammad Salam <mhd.salam99@gmail.com>
2022-09-11 14:20:40 +0200

    Pound-SEK converter

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -5,7 +5,7 @@ import java.text.*;
 public class CurrencyConverter {
     public static void main(String[] args) {
 
-        double amount, dollar, sek, code;
+        double amount, pound, sek, code;
 
         DecimalFormat f = new DecimalFormat("##.##");
 
@@ -13,15 +13,32 @@ public class CurrencyConverter {
 
         System.out.println("Which currency would you like to convert to?");
 
-        System.out.println("1: Dollar \t 2: SEK");
+        System.out.println("1: Pound \t 2: SEK");
         code = sc.nextInt();
 
         System.out.println("How much money would you like to convert?");
         amount = sc.nextFloat();
 
+        //amount conversion
         if (code == 1){
-            
+
+            pound = amount * 12;
+            System.out.println("Your " + amount + " pound is: " + f.format(pound) + " sek");
+
+
+        } else if (code == 2) {
+            //from sek to pound
+
+            sek = amount / 12;
+            System.out.println("Your " + amount + " sek is: " + f.format(sek) + " pound");
+
+
+        } else {
+            System.out.println("Invalid Input");
         }
+        System.out.println();
+        System.out.println("Thanks for using our conversion app!");
+
 
 
 
